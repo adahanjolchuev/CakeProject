a8ccc10fba8ff2d4a6c247af2d3a402a9b1d6677
adahan jolchuev <147965952+adahanjolchuev@users.noreply.github.com>
2024-03-12 15:00:54 +0600

    header (#1)

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import { CgProfile } from "react-icons/cg";
+import { FaHeart } from "react-icons/fa";
+import "./header.css";
+import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
+  const nav = useNavigate();
+  //adahan
   return (
-    <div>
-      
+    <div id="header">
+      <div className="container">
+        <div className="header">
+          <h1>
+            Сладости <br /> Амины
+          </h1>
+          <nav>
+            <Link to={'/'}>Home</Link>
+            <a href="#">About Us</a>
+            <a href="#">Cakes</a>
+            <a href="#">Contact</a>
+          </nav>
+          <div
+            className=""
+            style={{ display: "flex", gap: "20px", alignItems: "center" }}
+          >
+            <p>
+              <FaHeart />
+            </p>
+            <p onClick={() => nav("/admin")}>
+              <CgProfile />
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
